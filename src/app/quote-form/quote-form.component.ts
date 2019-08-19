c8959954b767c5a00093ba04a6b52919ac619fc2
lavylipesh <lilibethlangat@yahoo.com>
2019-08-18 23:37:03 -0400

    import Quote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,7 +1,7 @@
-/*import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import{Quote}from'../quote'
 
-@Component({
+/*@Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
