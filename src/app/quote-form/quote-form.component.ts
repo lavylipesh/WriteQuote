cde99124efc1d4d6ffcc9632c9ac773d3360f7f2
lavylipesh <lilibethlangat@yahoo.com>
2019-08-18 23:38:22 -0400

    like folder added to my app

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import{Quote}from'../quote'
 
-/*@Component({
+@Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-export class QuoteFormComponent implements OnInit {
+/*export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0,"","",new Date());
   @Output() addQuote = new EventEmitter<Quote>();
 
