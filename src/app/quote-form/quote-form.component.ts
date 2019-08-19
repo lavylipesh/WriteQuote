c0c43b51abadab4cfeabd2802d964233d9a7a47b
lavylipesh <lilibethlangat@yahoo.com>
2019-08-18 23:39:36 -0400

    add a function on button submit quote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -6,7 +6,7 @@ import{Quote}from'../quote'
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-/*export class QuoteFormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0,"","",new Date());
   @Output() addQuote = new EventEmitter<Quote>();
 
