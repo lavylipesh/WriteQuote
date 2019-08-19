b0c011553984ef2b3baac0c7c68c39874489ea5a
lavylipesh <lilibethlangat@yahoo.com>
2019-08-19 00:01:28 -0400

    add function delete quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -25,7 +25,7 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index){
     this.quote[index].showDescription = !this.quote[index].showDescription;
   }
-  /*deleteQuote(isComplete, index){
+  deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}?`)
       if(toDelete)
@@ -33,7 +33,7 @@ export class QuoteComponent implements OnInit {
     }
 
   }
-  numberOfLikes:number;
+  /*numberOfLikes:number;
   likeButtonClick(){
     this.numberOfLikes++;
   }
